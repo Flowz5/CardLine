1c6e5ed4e0a7b0276f8f6210d5b41a4051c0d01c
Léo Dupont <dupontleo999@gmail.com>
2026-02-11 11:56:56 +0100

    getters

--- animal.py
+++ animal.py
@@ -6,6 +6,21 @@ class Animal:
         self.poids = float(poids)
         self.longevite = int(longevite)
 
+    def getNom(self):
+        return self.nom
+    
+    def getNomSci(self):
+        return self.nom_scientifique
+    
+    def getTaille(self):
+        return self.taille
+    
+    def getPoids(self):
+        return self.poids
+    
+    def getLongevite(self):
+        return self.longevite
+
     def comparer(self, other: Animal) -> bool:
         return self.taille <= other.taille
 
